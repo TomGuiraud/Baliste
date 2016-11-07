71b9f7657333b5d2e475ee19ffd16f7ddb1a28b5
TomGuiraud <tom_guiraud@hotmail.fr>
2016-11-07 22:06:46 +0100

    Improve rotation of the balist

--- Baliste/Assets/Scripts/BalistBehaviour.cs
+++ Baliste/Assets/Scripts/BalistBehaviour.cs
@@ -34,6 +34,7 @@ public class BalistBehaviour : MonoBehaviour {
 	// Projectile Variables
 	public GameObject projectile;
 	public Transform projectileLaunchPoint;
+	public AnimationCurve _projectileCurve;
 	public float projectileStrenght;
 
 	//Mesh
@@ -68,20 +69,6 @@ public class BalistBehaviour : MonoBehaviour {
 
 	// MOVEMENT
 	public void BalistMovement (){
-	/*	if (Input.GetButton(_controls.BalistHorizontal)){
-			print ("Move Forward");
-		}else if (Input.GetButton(_controls.BalistHorizontal)){
-			print ("Move Backward");
-		}
-		if (Input.GetButton(_controls.BalistHorizontal)){
-			print ("Move Left");
-		}else if (Input.GetButton(_controls.BalistHorizontal)){
-			print ("Move Right");
-
-		}*/
-
-
-
 		//ZPosition
 		RaycastHit hit;
 		if (Physics.Raycast(transform.position, -Vector3.up, out hit)){
@@ -93,7 +80,6 @@ public class BalistBehaviour : MonoBehaviour {
 		//Balist Orientation
 		Vector3[] tmpWheelTargetPosArray = new Vector3[4];
 		for (int i = 0 ; i < _wheelsTranformArray.Length ; i++){
-			//Debug.DrawRay(_wheelsTranformArray[i].position, -Vector3.up,Color.white);
 			RaycastHit whit;
 			if (Physics.Raycast(_wheelsTranformArray[i].position + Vector3.up, -Vector3.up * 10, out whit)){
 				Vector3 tmpWheelTargetPos = whit.point;
@@ -101,43 +87,36 @@ public class BalistBehaviour : MonoBehaviour {
 			}
 		}
 
-		//print ("ArrayLenght : " + tmpWheelTargetPosArray.Length);
-
 		if (tmpWheelTargetPosArray.Length == 4) {
 			Vector3 side1 = tmpWheelTargetPosArray [0] - tmpWheelTargetPosArray [1];
 			Vector3 side2 = tmpWheelTargetPosArray [0] - tmpWheelTargetPosArray [2];
 			Vector3 perp1 = Vector3.Cross(side1, side2) * -1 ;
 			Vector3 tmpMiddlePoint1 = ((tmpWheelTargetPosArray [0] + tmpWheelTargetPosArray [1] + tmpWheelTargetPosArray [2])/3);
-			//Debug.DrawRay(tmpMiddlePoint1,perp1,Color.blue);
+			Debug.DrawRay(tmpMiddlePoint1,perp1,Color.blue);
 
 			Vector3 side3 = tmpWheelTargetPosArray [0] - tmpWheelTargetPosArray [1];
 			Vector3 side4 = tmpWheelTargetPosArray [0] - tmpWheelTargetPosArray [3];
 			Vector3 perp2 = Vector3.Cross(side3, side4) * -1;
 			Vector3 tmpMiddlePoint2 = ((tmpWheelTargetPosArray [0] + tmpWheelTargetPosArray [1] + tmpWheelTargetPosArray [3])/3);
-			//Debug.DrawRay(tmpMiddlePoint2, perp2,Color.red);
+			Debug.DrawRay(tmpMiddlePoint2, perp2,Color.red);
 
 			Vector3 side5 = tmpWheelTargetPosArray [1] - tmpWheelTargetPosArray [2];
 			Vector3 side6 = tmpWheelTargetPosArray [1] - tmpWheelTargetPosArray [3];
 			Vector3 perp3 = Vector3.Cross(side5, side6);
 			Vector3 tmpMiddlePoint3 = ((tmpWheelTargetPosArray [1] + tmpWheelTargetPosArray [2] + tmpWheelTargetPosArray [3])/3);
-			//Debug.DrawRay(tmpMiddlePoint3, perp3,Color.green);
+			Debug.DrawRay(tmpMiddlePoint3, perp3,Color.green);
 
 			Vector3 side7 = tmpWheelTargetPosArray [0] - tmpWheelTargetPosArray [2];
 			Vector3 side8 = tmpWheelTargetPosArray [0] - tmpWheelTargetPosArray [3];
 			Vector3 perp4 = Vector3.Cross(side7, side8);
 			Vector3 tmpMiddlePoint4 = ((tmpWheelTargetPosArray [0] + tmpWheelTargetPosArray [2] + tmpWheelTargetPosArray [3])/3);
-			//Debug.DrawRay(tmpMiddlePoint4, perp4,Color.yellow);
+			Debug.DrawRay(tmpMiddlePoint4, perp4,Color.yellow);
 
 			Vector3 tmpFinalRotationVector = ((perp1 + perp2 + perp3 + perp4) / 4);
 			Debug.DrawRay(this.transform.position, tmpFinalRotationVector ,Color.green);
 			Debug.DrawRay(this.transform.position, this.transform.up ,Color.red);
 
-			Quaternion tmpTargetRotation = Quaternion.FromToRotation(this.transform.up, tmpFinalRotationVector);
-			Quaternion tmpCurrentRot = this.transform.rotation;
-
-			//tmpTargetRotation.Set(tmpTargetRotation.x, this.transform.rotation.y, tmpTargetRotation.z, tmpTargetRotation.w);
-			//this.transform.up = Vector3.Lerp(this.transform.rotation.eulerAngles, tmpTargetRotation, Time.smoothDeltaTime * 50);
-			//this.transform.rotation = Quaternion.LerpUnclamped(this.transform.rotation, tmpTargetRotation, 1);
+			this.transform.rotation = Quaternion.FromToRotation(this.transform.up, tmpFinalRotationVector) * this.transform.rotation;
 		}
 
 		// Direction
